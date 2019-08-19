6d4c857caed5702e52ae1693a66e32546e091c56
DELL <DELL@DESKTOP-9TMQGJQ>
2019-08-19 22:36:40 +0200

    This is Java Swing GUI Snake Game Project

---
+++ src/snakeproject/TypeType.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakeproject;
+
+/**
+ *
+ * @author Nouran
+ */
+public class TypeType {
+    
+    public static int fruit = 2;
+    public static int Snakebody = 1;
+    public static int Empty = 1;
+    
+}
